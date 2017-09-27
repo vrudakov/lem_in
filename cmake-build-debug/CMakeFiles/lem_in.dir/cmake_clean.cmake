2f2d1a271489b5d21c86d018dbaf7037961114ca
VR <rudakov044@gmail.com>
2017-09-27 18:45:54 +0300

    add algo

---
+++ cmake-build-debug/CMakeFiles/lem_in.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lem_in.dir/main.c.o"
+  "CMakeFiles/lem_in.dir/checkers.c.o"
+  "CMakeFiles/lem_in.dir/serve.c.o"
+  "CMakeFiles/lem_in.dir/algo.c.o"
+  "lem_in.pdb"
+  "lem_in"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lem_in.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
